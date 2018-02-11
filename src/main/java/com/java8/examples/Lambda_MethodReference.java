710ad1db57b0329cc35176a61d1667c4cd822d3e
Kishore Sripada <sripadaskishore@gmail.com>
2018-02-11 22:24:39 +0800

    Lambda Method References
    Method Reference using Lambda in Java 8

---
+++ src/main/java/com/java8/examples/Lambda_MethodReference.java
@@ -0,0 +1,23 @@
+package com.java8.examples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda_MethodReference {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+
+        // Legacy/Traditional way
+        /*for ( int i =0 ; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }*/
+
+        // For Each
+        /*for( int value : list ) {
+            System.out.println(value);
+        }*/
+
+        // Method Interface
+        list.forEach(System.out::println);
+    }
+}
